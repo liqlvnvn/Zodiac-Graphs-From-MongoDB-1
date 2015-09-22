39f211a808f4d7b656849658012c3a301c8a70bb
liqlvnvn <alexandre.polyakov@gmail.com>
2015-09-22 15:18:33 +0300

    check if data directory are exist

--- src/Main.hs
+++ src/Main.hs
@@ -6,7 +6,7 @@ import Parsing.ParsingFromMongo ( parse1, parse2, parse3, parse4 )
 import ToFile                  -- ( convert1, convert2, convert3, convert4 )
 import SimpleAnalysis
 
-import System.FilePath          ( (</>) )
+import System.Directory
 
 main :: IO ()
 main = do
@@ -20,13 +20,16 @@ main = do
   let b = parse2 b'
   let c = parse3 c'
   let d = parse4 d'
+  -- Writing results
+  createDirectoryIfMissing False "data"
+  setCurrentDirectory "data"
   -- Make data files for gnuplot
-  writeFile ("data" </> "data1.txt") (convert1 a)
-  writeFile ("data" </> "data2.txt") (convert2 b)
-  writeFile ("data" </> "data3.txt") (convert3 c)
-  writeFile ("data" </> "data4.txt") (convert4 d)
+  writeFile "data1.txt" (convert1 a)
+  writeFile "data2.txt" (convert2 b)
+  writeFile "data3.txt" (convert3 c)
+  writeFile "data4.txt" (convert4 d)
   -- Analysis of graphs
-  writeFile ("data" </> "graph1.txt") (mkAnalysisOf1stGraph a)
-  writeFile ("data" </> "graph2.txt") (mkAnalysisOf2ndGraph b)
-  writeFile ("data" </> "graph3.txt") (mkAnalysisOf3rdGraph c)
-  writeFile ("data" </> "graph4.txt") (mkAnalysisOf4rdGraph d)
+  writeFile "graph1.txt" (mkAnalysisOf1stGraph a)
+  writeFile "graph2.txt" (mkAnalysisOf2ndGraph b)
+  writeFile "graph3.txt" (mkAnalysisOf3rdGraph c)
+  writeFile "graph4.txt" (mkAnalysisOf4rdGraph d)
